5b81657be8c37458c82035731c7838feb7d35422
Samuel Calabresi <samuel.calabresi@broadbean.com>
2021-02-21 21:02:35 +0100

    updating App.js test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="header">I love you babe.. 🥠 ❤️ 🥠</h1>
+      <h1 className="header">I love you babe..</h1>
+      <h1 className="header">🥠 ❤️ 🥠</h1>
     </div>
   );
 }
